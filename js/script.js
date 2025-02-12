e991876249695870e9c0a78601c2cdca520795bd
KLEVIN KLEDEN <92249272+CHELVYN01@users.noreply.github.com>
2025-02-12 16:16:23 +0700

    change file api key script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const apiKey = CONFIG.API_KEY;
+const apiKey = process.env.API_KEY || "default_key";
 const searchInput = document.querySelector(".input input");
 const searchIcon = document.querySelector(".bx-search");
 const searchFrame = document.getElementById("search_frame");
